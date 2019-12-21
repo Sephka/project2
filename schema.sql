079d696742e355b7105a0c023f6fc2d0a994c3f3
Sephka <32805735+Sephka@users.noreply.github.com>
2019-12-21 13:09:27 -0700

    Updates

---
+++ schema.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE "Brands" (
+    	"Brand" VARCHAR NOT NULL,
+    	"Country" VARCHAR,
+	"Whiskies" VARCHAR,
+	"Votes" VARCHAR,
+	"Rating" VARCHAR,
+	"WB_Ranking" VARCHAR NOT NULL
+);
+
+CREATE TABLE "Distillery" (
+    	"Company" VARCHAR NOT NULL,
+    	"Country" VARCHAR,
+	"Founded" VARCHAR,
+	"Closed" VARCHAR,
+	"Views" VARCHAR NOT NULL,
+	"Ranking" VARCHAR NOT NULL,
+	"Votes" VARCHAR,
+	"Wishlist" VARCHAR,
+    	"Rating" NUMERIC(5,2),
+	"Whisky" VARCHAR,
+	"Collection" VARCHAR,
+    	"Lat" INT NOT NULL,
+    	"Lon" INT NOT NULL
+);
+
