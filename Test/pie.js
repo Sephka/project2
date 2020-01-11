6a0ed7314bb20935c5a3564ee29ad04ef2602a5e
Morgan <mcordray88@protonmail.com>
2020-01-10 21:23:00 -0700

    switching branches

--- Test/pie.js
+++ Test/pie.js
@@ -4,6 +4,9 @@ var margin ={top:20, right: 20, bottom: 20, left:20}
     height = 500 - margin.top - margin.bottom,
     radius = width/2;
 
+
+
+
 //create arc generator and pie generator
 
 var arc = d3.arc()
@@ -31,15 +34,27 @@ var svg = d3.select("body")
 d3.csv("Whisky_Brand.csv").then(function(whiskeyData){
 //d3.csv("Whisky_Brand.csv", function(whiskeyData){
     whiskeyData.forEach(function(d){
-        d.Whiskies = +d.Whiskies;
-        d.Votes = +d.Country;
+        d.Brand = +d.Brand;
+        d.Votes = +d.Votes;
+        d.Country = +d.Country;
     });
 
-    var g = svg.selectAll(".arc")
+    var nested_data = d3.nest()
+    .key(function(d) { return d.Brand, d.Country; })
+    .entries(whiskeyData);
+    console.log(nested_data)
+
+    var g = svg
+        .selectAll(".country")
         .data(pie(whiskeyData))
         .enter()
         .append("g")
-        .attr("class", "arc");
+        .attr("class", "arc")
+    /*var g = svg.selectAll(".arc")
+        .data(pie(whiskeyData))
+        .enter()
+        .append("g")
+        .attr("class", "arc");*/
 
     //append path of arc
 
@@ -52,5 +67,19 @@ d3.csv("Whisky_Brand.csv").then(function(whiskeyData){
     g.append("text")
         .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
         .attr("dy", ".35em")
-        .text(function(d) {return d.whiskeyData.Country;} );
+        .text(function(d) {return d.Brand;} );
+/*
+        d3.select('g')
+        .selectAll('text')
+        .data(whiskeyData)
+        .enter()
+        .append('text')
+        .each(function(d) {
+            var centroid = labelArc.centroid(d);
+            d3.select(this)
+                .attr('x', centroid[0])
+                .attr('y', centroid[1])
+                .attr('dy', '0.33em')
+                .text(d.whiskeyData.Whiskies);*/
+        
 })
